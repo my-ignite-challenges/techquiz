4a510622f181e143233843d0684d09898f73a2ef
George W F Santos <gweverton21@hotmail.com>
2023-04-26 13:57:46 -0300

    implement sound and vibration feedback based on whether the answer is correct

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 
 import { useNavigation, useRoute } from "@react-navigation/native";
+import { Audio } from "expo-av";
+import * as Haptics from "expo-haptics";
 import { Alert, Text, View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
@@ -120,6 +122,17 @@ export function Quiz() {
     };
   });
 
+  async function playAudio(audioDescription: "correct" | "wrong") {
+    const file =
+      audioDescription === "correct"
+        ? require("../../assets/correct.mp3")
+        : require("../../assets/wrong.mp3");
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true });
+
+    await sound.setPositionAsync(0);
+    await sound.playAsync();
+  }
+
   function handleSkipConfirmation() {
     Alert.alert("Pular", "Deseja realmente pular a questão?", [
       { text: "Sim", onPress: () => handleNextQuestion() },
@@ -156,10 +169,12 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === selectedAnswer) {
-      setAnswerStatus(1);
       setPoints((prevState) => prevState + 1);
+      await playAudio("correct");
+      setAnswerStatus(1);
       handleNextQuestion();
     } else {
+      await playAudio("wrong");
       setAnswerStatus(2);
       shakeWrongAnswerCard();
     }
@@ -183,7 +198,9 @@ export function Quiz() {
     return true;
   }
 
-  function shakeWrongAnswerCard() {
+  async function shakeWrongAnswerCard() {
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+
     shake.value = withSequence(
       withTiming(3, { duration: 400, easing: Easing.bounce }),
       withTiming(0, undefined, (finished) => {
